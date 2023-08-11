19f0b103ba2e9a5768d7a0c6f9c5d58a4c6ab821
Darrell Lamm <darrelllamm0@gmail.com>
2023-08-11 09:41:16 -0400

    Initial commit

---
+++ DRLdefines.h
@@ -0,0 +1,20 @@
+// DRL: Begin
+#ifndef DRL_DEFINES_H
+#define DRL_DEFINES_H
+
+#define INSERT_IDLE
+#define FAKE_ACK
+
+#undef RESET_PREAMBLE_COUNT
+#define ADD_IDLE_PACKET
+// This may be a problem
+#define ADD_MSGS
+#define ADD_MSGS_EPS32
+
+#define ADD_DRIVER
+#define ADD_OLED
+
+#define ADD_JOIN
+
+#endif
+// DRL: End
