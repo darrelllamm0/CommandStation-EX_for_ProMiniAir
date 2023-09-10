209b68acbdbc3b1fd43c660126cf0bcc9547e699
Darrell Lamm <darrelllamm0@gmail.com>
2023-09-09 21:50:04 -0400

    Clean-up of bracketing comments. No functionality change.

--- mySetup.h
+++ mySetup.h
@@ -1,8 +1,8 @@
 #include "DRLDefines.h"
-// DRL: begin
+// DRL: Begin
 #if defined(ADD_JOIN)
 // Output Main on Programming Track output, Pin 13 based on my version of config.h
 #pragma message "Adding <1 JOIN> to startup"
 SETUP("<1 JOIN>");
 #endif
-// DRL: end
+// DRL: End
